62fb7fedc0539b1b7fd9156779558c4f053a6753
benson <benson5819+tw@gmail.com>
2022-05-25 00:55:16 +0800

    sync all member fields from propublica

--- scripts/testing/member-sync-one.ts
+++ scripts/testing/member-sync-one.ts
@@ -4,7 +4,13 @@ import { MemberResolver } from "../../src/resolver/member.resolver";
 // yarn ts-node ./scripts/testing/member-sync-one.ts
 
 if (require.main === module) {
-  new MemberResolver().syncMemberWithId('S000622', ['firstName']).then(l => {
+  new MemberResolver().syncMemberWithId('S000622').then(l => {
+    console.log("member-sync-one: S000622");
+    console.log(l);
+  });
+
+  new MemberResolver().syncMemberWithId('C000721').then(l => {
+    console.log("member-sync-one: C000721");
     console.log(l);
   })
 }
--- src/data-sync/member.sync.ts
+++ src/data-sync/member.sync.ts
@@ -1,5 +1,5 @@
 import { intersection } from "lodash";
-import { Member } from "../../common/models";
+import { Member, MemberRole } from "../../common/models";
 import { EntitySyncer, } from "./entity.sync";
 import { ProPublicaHelper } from "./sources/propublica";
 
@@ -26,10 +26,119 @@ export class MemberSyncer extends EntitySyncer<Member> {
 
 class MemberProPublicaSyncer extends EntitySyncer<Member> {
   public async sync(): Promise<Member> {
-    if (this.fields == null || this.fields.includes('firstName')) {
-      const result = await ProPublicaHelper.get(`https://api.propublica.org/congress/v1/members/${this.entity.id}.json`);
-      this.entity.firstName = result[0]['first_name'];
+    const proPublicaResult = await ProPublicaHelper.get(`https://api.propublica.org/congress/v1/members/${this.entity.id}.json`);
+
+    if (proPublicaResult[0]['first_name']) {
+      this.entity.firstName = proPublicaResult[0]['first_name'];
     }
+
+    if (proPublicaResult[0]['middle_name']) {
+      this.entity.middleName = proPublicaResult[0]['middle_name'];
+    }
+
+    if (proPublicaResult[0]['last_name']) {
+      this.entity.lastName = proPublicaResult[0]['last_name'];
+    }
+
+    if (proPublicaResult[0]['suffix']) {
+      this.entity.nameSuffix = proPublicaResult[0]['suffix'];
+    }
+
+    // if (this.fields?.includes('nickname')) {
+    //   this.entity.nickname = ?
+    // }
+
+    if (proPublicaResult[0]['gender']) {
+      const gender = proPublicaResult[0]['gender'];
+
+      if (gender === 'M') {
+        this.entity.gender = 'male';
+      } else if (gender === 'F') {
+        this.entity.gender = 'female';
+      }
+    }
+
+    if (proPublicaResult[0]['date_of_birth']) {
+      this.entity.birthday = proPublicaResult[0]['date_of_birth'];
+    }
+
+    // if (this.fields?.includes('website')) {
+    //   this.entity.website = ?
+    // }
+
+    // if (this.fields?.includes('office')) {
+    //   this.entity.office = ?
+    // }
+
+    // if (this.fields?.includes('phone')) {
+    //   this.entity.phone = ?
+    // }
+
+    if (proPublicaResult[0]['cspan_id']) {
+      this.entity.cspanId = proPublicaResult[0]['cspan_id'];
+    }
+
+    if (proPublicaResult[0]['twitter_account']) {
+      this.entity.twitterId = proPublicaResult[0]['twitter_account'];
+    }
+
+    if (proPublicaResult[0]['facebook_account']) {
+      this.entity.facebookId = proPublicaResult[0]['facebook_account'];
+    }
+
+    if (proPublicaResult[0]['youtube_account']) {
+      this.entity.youtubeId = proPublicaResult[0]['youtube_account'];
+    }
+
+    // if (this.fields?.includes('profilePictureUri')) {
+    //   this.entity.profilePictureUri = ?
+    // }
+
+    if (proPublicaResult[0]['roles'].length > 0) {
+      const roles = proPublicaResult[0]['roles'];
+
+      for (let role_idx = 0; role_idx < roles.length; role_idx++) {
+        const role = roles[role_idx];
+
+        if (this.entity.congressRoles?.some((congressRole: MemberRole): boolean =>
+          congressRole.congressNumbers.some((congressNum: number): boolean =>
+            congressNum === Number(role['congress'])))
+        ) {
+          // this role exists => update the data
+          console.log(`role: ${Number(role['congress'])} exists`);
+        } else {
+          // this role doesn't exist => push a new one
+          if (!this.entity.congressRoles) {
+            this.entity.congressRoles = [];
+          }
+
+          if (role['chamber'] === 'Senate') {
+            this.entity.congressRoles.push({
+              congressNumbers: [Number(role['congress'])],
+              chamber: 's',
+              startDate: role['start_date'],
+              endDate: role['end_date'],
+              party: role['party'],
+              state: role['state'],
+              senatorClass: role['senate_class']
+            });
+          } else {
+            this.entity.congressRoles.push({
+              congressNumbers: [Number(role['congress'])],
+              chamber: 'h',
+              startDate: role['start_date'],
+              endDate: role['end_date'],
+              party: role['party'],
+              state: role['state'],
+              district: role['district']
+            });
+          }
+        }
+      }
+    }
+
     return this.entity;
   }
+
+  // TODO: party & state mapping from propublica to member type
 }
