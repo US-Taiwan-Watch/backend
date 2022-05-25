94ebe063dba49afb89552d663b3e6b2bc8a779ed
jingwan <hsugaigai@gmail.com>
2022-05-25 15:58:04 -0700

    Swap member-sync-all and member-sync - it was wrong previously

--- scripts/member-sync-all.ts
+++ scripts/member-sync-all.ts
@@ -2,13 +2,13 @@ import 'reflect-metadata';
 import { MemberResolver } from "../src/resolver/member.resolver";
 
 /**
- * yarn ts-node ./scripts/member-sync.ts
+ * yarn ts-node ./scripts/member-sync-all.ts
  *
  * This script will be run on a regular basis to sync all updated members
  */
 
 if (require.main === module) {
-  new MemberResolver().fetchAndSyncUpdatedMembers().then(_ => {
+  new MemberResolver().fetchAndSyncAllMembers().then(_ => {
     console.log('sync succcessfully!');
   })
 }
--- scripts/member-sync.ts
+++ scripts/member-sync.ts
@@ -2,14 +2,14 @@ import 'reflect-metadata';
 import { MemberResolver } from "../src/resolver/member.resolver";
 
 /**
- * yarn ts-node ./scripts/member-sync-all.ts
+ * yarn ts-node ./scripts/member-sync.ts
  *
  * This script will be run manually to sync all members in U.S. history
  */
 
 if (require.main === module) {
-  new MemberResolver().fetchAndSyncAllMembers().then(_ => {
-    console.log('sync succcessfully!');
+  new MemberResolver().fetchAndSyncUpdatedMembers().then(l => {
+    console.log(l);
   })
 }
 
