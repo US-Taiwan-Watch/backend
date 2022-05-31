f26a66943cb98981c664a6d0f80001c3c9f0f8aa
benson <benson5819+tw@gmail.com>
2022-05-31 08:20:55 +0800

    add sync-recent script for sync latest 2 congress term

---
+++ scripts/member-sync-recent.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import { MemberResolver } from "../src/resolver/member.resolver";
+
+/**
+ * yarn ts-node ./scripts/member-sync-recent.ts
+ *
+ * This script will be run on a regular basis to
+ *    (1) fetch from database and, to
+ *    (2) sync members of the latest 2 terms for both senate and house and then, to
+ *    (3) update to database if any difference exists
+ */
+
+if (require.main === module) {
+  const currYear = new Date().getFullYear();
+  const currCongress = 117 + Math.floor((currYear - 2021) / 2);   // check for session start data: Jan. 3?
+
+  // Sync for latest 2 terms of Senate
+  new MemberResolver().fetchAndSyncMemberByCongress('senate', currCongress - 1).then(_ => {
+    console.log(`sync senate ${currCongress - 1} succcessfully!`);
+  });
+
+  new MemberResolver().fetchAndSyncMemberByCongress('senate', currCongress).then(_ => {
+    console.log(`sync senate ${currCongress} succcessfully!`);
+  });
+
+  // Sync for latest 2 terms of House
+  new MemberResolver().fetchAndSyncMemberByCongress('house', currCongress - 1).then(_ => {
+    console.log(`sync house ${currCongress - 1} succcessfully!`);
+  });
+
+  new MemberResolver().fetchAndSyncMemberByCongress('house', currCongress).then(_ => {
+    console.log(`sync house ${currCongress} succcessfully!`);
+  });
+}
+
--- scripts/testing/member-sync-one.ts
+++ scripts/testing/member-sync-one.ts
@@ -4,12 +4,12 @@ import { MemberResolver } from "../../src/resolver/member.resolver";
 // yarn ts-node ./scripts/testing/member-sync-one.ts
 
 if (require.main === module) {
-  new MemberResolver().syncMemberWithId('S000622').then(l => {
+  new MemberResolver().fetchAndSyncMemberById('S000622').then(l => {
     console.log("member-sync-one: S000622");
     console.log(l);
   });
 
-  new MemberResolver().syncMemberWithId('C000721').then(l => {
+  new MemberResolver().fetchAndSyncMemberById('C000721').then(l => {
     console.log("member-sync-one: C000721");
     console.log(l);
   })
